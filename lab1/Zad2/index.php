8dc85bd908aadae7f0807713f6a8702c4b011075
Daria Szablowska <s24967@pjwstk.edu.pl>
2023-03-22 14:25:34 +0100

    Zadanie 2

---
+++ lab1/Zad2/index.php
@@ -0,0 +1,10 @@
+<?php
+echo "Podaj długość boku a: ";
+$a = readline("Podaj długość boku a: ");
+echo "Podaj długość boku b: ";
+$b = readline("Podaj długość boku b: ");
+
+$pole = $a * $b;
+
+echo "Pole prostokąta o bokach $a i $b wynosi: $pole";
+?>
