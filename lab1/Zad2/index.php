d0e68771aa810d1471d6023c3ca6b8f436f85a18
Daria Szablowska <s24967@pjwstk.edu.pl>
2023-03-24 12:25:53 +0100

    Zadanie2 - poprawa

--- lab1/Zad2/index.php
+++ lab1/Zad2/index.php
@@ -1,10 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Kalkulator pola prostokąta</title>
+</head>
+<body>
+<h1>Kalkulator pola prostokąta</h1>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    Bok a: <input type="number" name="a"><br><br>
+    Bok b: <input type="number" name="b"><br><br>
+    <input type="submit" name="submit" value="Oblicz">
+</form>
+
 <?php
-echo "Podaj długość boku a: ";
-$a = readline("Podaj długość boku a: ");
-echo "Podaj długość boku b: ";
-$b = readline("Podaj długość boku b: ");
-
-$pole = $a * $b;
-
-echo "Pole prostokąta o bokach $a i $b wynosi: $pole";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $a = $_POST["a"];
+    $b = $_POST["b"];
+    $pole = $a * $b;
+    echo "<h2>Pole prostokąta o bokach $a i $b wynosi: $pole</h2>";
+}
 ?>
+</body>
+</html>
