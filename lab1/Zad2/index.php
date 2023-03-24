6577cd028bccd3c6a7871232b335e78cab2b6ea4
Daria Szablowska <s24967@pjwstk.edu.pl>
2023-03-24 14:11:24 +0100

    takie tam kosmetyczne zmiany

--- lab1/Zad2/index.php
+++ lab1/Zad2/index.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Kalkulator pola prostokąta</title>
+    <title>Zadanie 2</title>
 </head>
 <body>
 <h1>Kalkulator pola prostokąta</h1>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<form method="post">
     Bok a: <input type="number" name="a"><br><br>
     Bok b: <input type="number" name="b"><br><br>
     <input type="submit" name="submit" value="Oblicz">
 </form>
 
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
     $a = $_POST["a"];
     $b = $_POST["b"];
     $pole = $a * $b;
     echo "<h2>Pole prostokąta o bokach $a i $b wynosi: $pole</h2>";
-}
 ?>
 </body>
 </html>
